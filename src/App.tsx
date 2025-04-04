a1046b948b68b562dad7ee298b1e1f115da5a157
anirudhpviic <anirudh.pv.iic.0106@example.com>
2025-04-04 15:03:17 +0530

    update: private-route-auth-check

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,77 @@ import {
 } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import { jwtDecode } from "jwt-decode";
+import axios from "axios";
 
 const PrivateRoute = ({ children }: { children: any }) => {
   const [isAuthenticated, setIsAuthenticated] = useState<null | boolean>(null);
 
-  useEffect(() => {
-    chrome.storage.local.get("accessToken", (result) => {
-      setIsAuthenticated(!!result.accessToken);
+  const getToken = async (key: string): Promise<string | null> => {
+    return new Promise((resolve) => {
+      chrome.storage.local.get([key], (result) => {
+        resolve(result[key] || null);
+      });
     });
+  };
+
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      const accessToken = await getToken("accessToken");
+
+      if (!accessToken) {
+        chrome.storage.local.clear();
+        setIsAuthenticated(false);
+        return;
+      }
+
+      const decoded: { exp: number } = jwtDecode(accessToken);
+      const isExpired = decoded.exp * 1000 - 5 * 60 * 1000 < Date.now(); // Subtract 5 minutes from expiration time
+
+      if (!isExpired) {
+        setIsAuthenticated(true);
+        return;
+      }
+
+      const refreshToken = await getToken("refreshToken");
+
+      if (!refreshToken) {
+        chrome.storage.local.clear();
+        setIsAuthenticated(false);
+        return;
+      }
+
+      try {
+        const res = await axios.post(
+          `${import.meta.env.VITE_BACKEND_URI}/auth/refresh-tokens`,
+          { refreshToken }
+        );
+
+        const newAccessToken = res?.data?.data?.accessToken;
+        const newRefreshToken = res?.data?.data?.refreshToken;
+
+        await chrome.storage.local.set({
+          accessToken: newAccessToken,
+          refreshToken: newRefreshToken,
+        });
+
+        setIsAuthenticated(true);
+      } catch (error) {
+        chrome.storage.local.clear();
+        setIsAuthenticated(false);
+      }
+    };
+
+    checkAuthentication();
   }, []);
 
-  if (isAuthenticated === null) return <div>Loading...</div>;
+  if (isAuthenticated === null) {
+    return (
+      <div className="flex items-center justify-center">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
   return isAuthenticated ? children : <Navigate to="/login" />;
 };
